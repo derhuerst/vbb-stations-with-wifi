1ba263b297eb8dee987118babbdeeb7e47bf91b0
Jannis R <mail@jannisr.de>
2018-10-14 18:47:45 +0200

    ap.line + ap.nextStation -> ap.lines: [[line, nextStation]]

--- index.js
+++ index.js
@@ -18,36 +18,40 @@ module.exports = {
 		accessPoints: [
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000009101', // U Amrumer Str.
+				lines: [
+					['U9', '900000009101'] // U Amrumer Str.
+				],
 				position: 0.25, // todo: this is inexact
 				bssid: '00:f6:63:cc:00:9f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000009101', // U Amrumer Str.
+				lines: [
+					['U9', '900000009101'] // U Amrumer Str.
+				],
 				position: 0.6, // todo: this is inexact
 				bssid: '00:81:c4:f6:12:8f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000009201', // U Nauener Platz
+				lines: [
+					['U9', '900000009201'] // U Nauener Platz
+				],
 				position: 0.25, // todo: this is inexact
 				bssid: '00:f6:63:cc:26:af',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000009201', // U Nauener Platz
+				lines: [
+					['U9', '900000009201'] // U Nauener Platz
+				],
 				position: 0.5, // todo: this is inexact
 				bssid: '00:81:c4:e7:0e:0f',
 				mac: null
@@ -60,36 +64,40 @@ module.exports = {
 		accessPoints: [
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000001201', // S+U Westhafen
+				lines: [
+					['U9', '900000001201'] // S+U Westhafen
+				],
 				position: 0.5,
 				bssid: '00:81:c4:93:c6:ef',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000001201', // S+U Westhafen
+				lines: [
+					['U9', '900000001201'] // S+U Westhafen
+				],
 				position: 0.75, // todo: this is inexact
 				bssid: '00:81:c4:93:c5:cf',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000009102', // U Leopoldplatz
+				lines: [
+					['U9', '900000009102'] // U Leopoldplatz
+				],
 				position: 0.5,
 				bssid: '00:81:c4:90:cf:4f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000009102', // U Leopoldplatz
+				lines: [
+					['U9', '900000009102'] // U Leopoldplatz
+				],
 				position: 0.75, // todo: this is inexact
 				bssid: '00:81:c4:91:06:7f',
 				mac: null
@@ -108,45 +116,50 @@ module.exports = {
 		accessPoints: [
 			{
 				type: 'platform',
-				line: 'U2',
 				platform: null,
-				nextStation: '900000023101', // U Ernst-Reuter-Platz
+				lines: [
+					['U2', '900000023101'] // U Ernst-Reuter-Platz
+				],
 				position: 0.5, // todo: this is inexact
 				bssid: '00:f6:63:e1:41:2f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000003101', // U Hansaplatz
+				lines: [
+					['U9', '900000003101'] // U Hansaplatz
+				],
 				position: 0.1,
 				bssid: '00:f6:63:cb:fe:4f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000003101', // U Hansaplatz
+				lines: [
+					['U9', '900000003101'] // U Hansaplatz
+				],
 				position: 0.5,
 				bssid: '00:f6:63:af:fb:0f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000023203', // U Kurfürstendamm
+				lines: [
+					['U9', '900000023203'] // U Kurfürstendamm
+				],
 				position: 0.25,
 				bssid: '00:f6:63:af:fa:4f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000023203', // U Kurfürstendamm
+				lines: [
+					['U9', '900000023203'] // U Kurfürstendamm
+				],
 				position: 0.8,
 				bssid: '00:f6:63:9f:3c:9f',
 				mac: null
@@ -160,18 +173,20 @@ module.exports = {
 		accessPoints: [
 			{
 				type: 'platform',
-				line: 'U2',
 				platform: null,
-				nextStation: '900000022201', // U Deutsche Oper
+				lines: [
+					['U2', '900000022201'] // U Deutsche Oper
+				],
 				position: 0.35,
 				bssid: '00:81:c4:e7:0d:8f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U2',
 				platform: null,
-				nextStation: '900000022201', // U Deutsche Oper
+				lines: [
+					['U2', '900000022201'] // U Deutsche Oper
+				],
 				position: 0.65,
 				bssid: '00:81:c4:e7:0e:7f',
 				mac: null
@@ -197,36 +212,40 @@ module.exports = {
 		accessPoints: [
 			{
 				type: 'platform',
-				line: 'U2',
 				platform: null,
-				nextStation: '900000100012', // U Hausvogteiplatz
+				lines: [
+					['U2', '900000100012'] // U Hausvogteiplatz
+				],
 				position: 0,
 				bssid: '00:81:c4:f6:0f:4f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U2',
 				platform: null,
-				nextStation: '900000100012', // U Hausvogteiplatz
+				lines: [
+					['U2', '900000100012'] // U Hausvogteiplatz
+				],
 				position: 0.5,
 				bssid: '00:81:c4:9d:b4:8f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U2',
 				platform: null,
-				nextStation: '900000100010', // U Mohrenstr.
+				lines: [
+					['U2', '900000100010'] // U Mohrenstr.
+				],
 				position: 0.2,
 				bssid: '00:81:c4:33:26:ff',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U2',
 				platform: null,
-				nextStation: '900000100010', // U Mohrenstr.
+				lines: [
+					['U2', '900000100010'] // U Mohrenstr.
+				],
 				position: 0.75,
 				bssid: '00:81:c4:93:c2:5f',
 				mac: null
@@ -245,9 +264,10 @@ module.exports = {
 			},
 			{
 				type: 'platform',
-				line: 'U6',
 				platform: null,
-				nextStation: '900000100027', // U Französische Str.
+				lines: [
+					['U6', '900000100027'] // U Französische Str.
+				],
 				position: 0.9,
 				bssid: '00:81:c4:9d:b5:b0',
 				mac: null
@@ -260,18 +280,20 @@ module.exports = {
 		accessPoints: [
 			{
 				type: 'platform',
-				line: 'U6',
 				platform: null,
-				nextStation: '900000100011', // U Stadtmitte
+				lines: [
+					['U6', '900000100011'] // U Stadtmitte
+				],
 				position: 0.7,
 				bssid: '00:81:c4:91:08:60',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U6',
 				platform: null,
-				nextStation: '900000100001', // S+U Friedrichstr.
+				lines: [
+					['U6', '900000100001'] // S+U Friedrichstr.
+				],
 				position: 0.3,
 				bssid: '00:81:c4:91:08:6f',
 				mac: null
@@ -284,36 +306,40 @@ module.exports = {
 		accessPoints: [
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000061102', // U Friedrich-Wilhelm-Platz
+				lines: [
+					['U9', '900000061102'] // U Friedrich-Wilhelm-Platz
+				],
 				position: 0.6,
 				bssid: '00:81:c4:f8:84:bf',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000061102', // U Friedrich-Wilhelm-Platz
+				lines: [
+					['U9', '900000061102'] // U Friedrich-Wilhelm-Platz
+				],
 				position: 1,
 				bssid: '00:81:c4:21:59:df',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000062203', // U Schloßstr.
+				lines: [
+					['U9', '900000062203'] // U Schloßstr.
+				],
 				position: 0.4,
 				bssid: '00:81:c4:f8:84:4f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000062203', // U Schloßstr.
+				lines: [
+					['U9', '900000062203'] // U Schloßstr.
+				],
 				position: 1,
 				bssid: '00:81:c4:e6:70:8f',
 				mac: null
@@ -326,72 +352,80 @@ module.exports = {
 		accessPoints: [
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000043201', // U Güntzelstr.
+				lines: [
+					['U9', '900000043201'] // U Güntzelstr.
+				],
 				position: 0.25,
 				bssid: '00:81:c4:e6:6c:df',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000043201', // U Güntzelstr.
+				lines: [
+					['U9', '900000043201'] // U Güntzelstr.
+				],
 				position: 0.75,
 				bssid: '00:81:c4:f6:15:e0',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000044202', // S+U Bundesplatz
+				lines: [
+					['U9', '900000044202'] // S+U Bundesplatz
+				],
 				position: 0.25,
 				bssid: '00:81:c4:e6:aa:1f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U9',
 				platform: null,
-				nextStation: '900000044202', // S+U Bundesplatz
+				lines: [
+					['U9', '900000044202'] // S+U Bundesplatz
+				],
 				position: 0.8,
 				bssid: '00:81:c4:9d:9c:af',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U7',
 				platform: null,
-				nextStation: '900000055102', // U Bayerischer Platz
+				lines: [
+					['U7', '900000055102'] // U Bayerischer Platz
+				],
 				position: 0.35,
 				bssid: '00:81:c4:9d:b5:df',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U7',
 				platform: null,
-				nextStation: '900000055102', // U Bayerischer Platz
+				lines: [
+					['U7', '900000055102'] // U Bayerischer Platz
+				],
 				position: 0.9,
 				bssid: '00:81:c4:6e:64:7f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U7',
 				platform: null,
-				nextStation: '900000041102', // U Blissestr.
+				lines: [
+					['U7', '900000041102'] // U Blissestr.
+				],
 				position: 0.3,
 				bssid: '00:81:c4:f6:1b:3f',
 				mac: null
 			},
 			{
 				type: 'platform',
-				line: 'U7',
 				platform: null,
-				nextStation: '900000041102', // U Blissestr.
+				lines: [
+					['U7', '900000041102'] // U Blissestr.
+				],
 				position: 0.9,
 				bssid: '00:81:c4:f8:c4:0f',
 				mac: null
--- readme.md
+++ readme.md
@@ -29,17 +29,19 @@ console.log(stations['900000023101']) // U Ernst-Reuter-Platz
 	wifi: true,
 	accessPoints: [ {
 		type: 'platform',
-		line: 'U2',
 		platform: null,
-		nextStation: '900000022201',
+		lines: [
+			['U2', '900000022201'] // U Deutsche Oper
+		],
 		position: 0.35,
 		bssid: '00:81:c4:e7:0d:8f',
 		mac: null
 	}, {
 		type: 'platform',
-		line: 'U2',
 		platform: null,
-		nextStation: '900000022201',
+		lines: [
+			['U2', '900000022201'] // U Deutsche Oper
+		],
 		position: 0.65,
 		bssid: '00:81:c4:e7:0e:7f',
 		mac: null
@@ -69,9 +71,10 @@ console.log(byBssid['00:81:c4:e7:0e:7f'])
 {
 	station: '900000023101',
 	type: 'platform',
-	line: 'U2',
 	platform: null,
-	nextStation: '900000022201',
+	lines: [
+		['U2', '900000022201'] // U Deutsche Oper
+	],
 	position: 0.65,
 	bssid: '00:81:c4:e7:0e:7f',
 	mac: null
@@ -97,14 +100,15 @@ An item in `accessPoints` should either have `type: 'platform'` (an access point
 
 | key | description | required | example |
 | -------- | ----------- | -------- | ------- |
-| `line` | the line running at the platform | yes | `'U2'` |
 | `platform` | the name of the platform | no | `'3a'` |
-| `nextStation` | ID of the next station on the line (use [`vbb-stations-cli`](https://npmjs.com/package/vbb-stations-cli)) | yes | `'900000022101'` |
+| `lines` | an array of pairs: 1) a line and 2) the ID of the next station on this line | yes | `[['U2', '900000022201']]` |
 | `position`| position of the access point at the platform, between `0` (rear end of the station) and `1` (front end of the station) ¹ | yes | `0.2` |
 | `bssid`| [BSSID](https://en.wikipedia.org/wiki/Service_set_(802.11_network)#Basic_service_sets_(BSSs)), lower case | yes | `'00:f6:63:cc:00:9f'` |
 | `mac` | [MAC address](https://en.wikipedia.org/wiki/MAC_address), lower case | no | `'00:f6:63:cc:00:9f'` |
 
 ¹ "front end" is the end *of the station* that the a vehicle running at the platform points to (*not* where the "front end" of the vehicle will be).
+
+*Pro tip:* Use [`vbb-stations-cli`](https://npmjs.com/package/vbb-stations-cli) to find station IDs.
 
 
 ## Contributing
